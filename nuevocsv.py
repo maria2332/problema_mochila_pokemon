82d52c8b11fd4a444f2a5a293169cb8ce401ff38
maria2332 <maria.ballesteros2004@gmail.com>
2023-05-04 10:11:43 +0200

    cambio

--- nuevocsv.py
+++ nuevocsv.py
@@ -11,3 +11,16 @@ df_NOMBRE_TOTAL.to_csv("pokedex_NOMBRE_TOTAL.csv", index=False)
 
 # Imprime un mensaje de confirmación.
 print("El archivo CSV con las columnas 'Nombre' y 'Total' ha sido guardado con éxito.")
+
+
+df_pokedex_actualizado_sin_columnas = pd.read_csv('pokedex_actualizado_sin_columnas.csv')
+
+# Añadir la columna "capture_rate" del dataframe "pokemon" al dataframe "df_pokedex_actualizado_sin_columnas"
+df_pokemon = pd.read_csv('pokemon.csv')
+df_pokedex_actualizado_sin_columnas['capture_rate'] = df_pokemon['capture_rate']
+
+# Seleccionar solo las columnas "NOMBRE" y "TOTAL"
+df_NOMBRE_TOTAL = df_pokedex_actualizado_sin_columnas[['NOMBRE', 'TOTAL']]
+
+# Guardar el resultado en un nuevo archivo CSV
+df_NOMBRE_TOTAL.to_csv('pokedex_NOMBRE_TOTAL.csv', index=False)
